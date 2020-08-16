90ae6d1cf2cacd0f46153070310b1f578db8d3bc
oSumAtrIX <johan.melkonyan1@web.de>
2020-08-16 02:38:02 +0200

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -6,46 +6,37 @@ using System.Runtime.CompilerServices;
 using dnlib.DotNet;
 using dnlib.DotNet.Emit;
 
-namespace SRXDLadder_Stringdec
-{
-	internal class Program
-	{
-		private static void Main(string[] args)
-		{
-			string dll = args[0];
-			Assembly reflectedAssembly = Assembly.UnsafeLoadFrom(dll);
-			ModuleDefMD moduleDef = ModuleDefMD.Load(dll, null);
-			Module asmbMod = reflectedAssembly.Modules.First<Module>();
-			int decryptionTypeMDToken = reflectedAssembly.GetTypes().FirstOrDefault(x => x.Namespace.Contains("PrivateImpl")).MetadataToken;
-			TypeDef decryptionTypeMethodDef = moduleDef.ResolveTypeDef(MDToken.ToRID(decryptionTypeMDToken));
-			IEnumerable<TypeDef> types = moduleDef.GetTypes();
-			foreach (TypeDef type in types)
-			{
-				foreach (MethodDef md in type.Methods)
-				{
-					bool flag = !md.HasBody;
-					if (!flag)
-					{
-						IList<Instruction> instructions = md.Body.Instructions;
-						for (int i = 0; i < instructions.Count; i++)
-						{
-							Instruction instruction = instructions[i];
-							bool flag2 = instruction.OpCode.Code != Code.Call || !decryptionTypeMethodDef.Methods.Contains(instruction.Operand);
-							if (!flag2)
-							{
-								MethodBase invokeMd = asmbMod.ResolveMethod(((MethodDef)instruction.Operand).MDToken.ToInt32());
-								bool flag3 = invokeMd.GetParameters().Length != 0;
-								if (!flag3)
-								{
-									instruction.OpCode = OpCodes.Ldstr;
-									instruction.Operand = (string)invokeMd.Invoke(null, new object[0]);
-								}
-							}
-						}
-					}
-				}
-			}
-			moduleDef.Write("SRXDLadder-stringdec.dll");
-		}
-	}
+namespace SRXDLadder_Stringdec {
+  internal class Program {
+    private static void Main(string[] args) {
+      string dll = args[0];
+      Assembly reflectedAssembly = Assembly.UnsafeLoadFrom(dll);
+      ModuleDefMD moduleDef = ModuleDefMD.Load(dll, null);
+      Module asmbMod = reflectedAssembly.Modules.First < Module > ();
+      int decryptionTypeMDToken = reflectedAssembly.GetTypes().FirstOrDefault(x =>x.Namespace.Contains("PrivateImpl")).MetadataToken;
+      TypeDef decryptionTypeMethodDef = moduleDef.ResolveTypeDef(MDToken.ToRID(decryptionTypeMDToken));
+      IEnumerable < TypeDef > types = moduleDef.GetTypes();
+      foreach(TypeDef type in types) {
+        foreach(MethodDef md in type.Methods) {
+          bool flag = !md.HasBody;
+          if (!flag) {
+            IList < Instruction > instructions = md.Body.Instructions;
+            for (int i = 0; i < instructions.Count; i++) {
+              Instruction instruction = instructions[i];
+              bool flag2 = instruction.OpCode.Code != Code.Call || !decryptionTypeMethodDef.Methods.Contains(instruction.Operand);
+              if (!flag2) {
+                MethodBase invokeMd = asmbMod.ResolveMethod(((MethodDef) instruction.Operand).MDToken.ToInt32());
+                bool flag3 = invokeMd.GetParameters().Length != 0;
+                if (!flag3) {
+                  instruction.OpCode = OpCodes.Ldstr;
+                  instruction.Operand = (string) invokeMd.Invoke(null, new object[0]);
+                }
+              }
+            }
+          }
+        }
+      }
+      moduleDef.Write("SRXDLadder-stringdec.dll");
+    }
+  }
 }
